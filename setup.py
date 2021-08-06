b5e3107bca1da353b3af1ec7129e0074b1e96d39
hmiao <739025250@qq.com>
2021-08-06 18:25:39 +0800

    [feature] change package name to delta_task

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# type: ignore
 # -*- coding: utf-8 -*-
 import shlex
 import sys
@@ -7,7 +8,7 @@ from setuptools.command.test import test as TestCommand
 
 
 class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+    user_options = [("pytest-args=", "a", "Arguments to pass to py.test")]
 
     def initialize_options(self):
         TestCommand.initialize_options(self)
@@ -22,22 +23,30 @@ class PyTest(TestCommand):
     def run_tests(self):
         # import here, cause outside the eggs aren't loaded
         import pytest
+
         errno = pytest.main(self.pytest_args)
         sys.exit(errno)
 
 
 setup(
-    name="delta",
+    name="delta_task",
     version="0.0.1",
     packages=find_packages(),
     include_package_data=True,
-    exclude_package_data={'': ['.gitignore']},
-    tests_require=['pytest'],
-    cmdclass={'test': PyTest},
+    exclude_package_data={"": [".gitignore"]},
+    install_requires=[
+        'numpy~=1.20.1',
+        'requests~=2.25.1',
+        'pytest~=6.2.4',
+        'dill~=0.3.4',
+    ],
+    tests_require=["pytest"],
+    cmdclass={"test": PyTest},
     test_suite="tests",
     zip_safe=False,
     author="miaohong",
     author_email="73902525@qq.com",
     description="delta framework",
-    python_requires='>=3.6',
+    python_requires=">=3.6",
+    url="https://github.com/delta-mpc/delta"
 )
