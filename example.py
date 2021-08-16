d1f7d5bb8cf9874b94b929eca706869a87aa91d6
hmiao <739025250@qq.com>
2021-08-16 19:55:59 +0800

    [feature] example

---
+++ example.py
@@ -0,0 +1,72 @@
+import torch
+import numpy as np
+import logging
+
+from delta import DeltaNode
+from delta.task import LearningTask
+from delta.node import DebugNode
+
+
+class LeNet(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = torch.nn.Conv2d(1, 16, 5, padding=2)
+        self.pool1 = torch.nn.AvgPool2d(2, stride=2)
+        self.conv2 = torch.nn.Conv2d(16, 16, 5)
+        self.pool2 = torch.nn.AvgPool2d(2, stride=2)
+        self.dense1 = torch.nn.Linear(400, 100)
+        self.dense2 = torch.nn.Linear(100, 10)
+
+    def forward(self, x: torch.Tensor):
+        x = self.conv1(x)
+        x = torch.relu(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = torch.relu(x)
+        x = self.pool2(x)
+    
+        x = x.view(-1, 400)
+        x = self.dense1(x)
+        x = torch.relu(x)
+        x = self.dense2(x)
+        return x
+
+
+model = LeNet()
+loss = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(
+    model.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-3, nesterov=True
+)
+
+
+def train_step(batch, model=model, loss=loss, optimizer=optimizer):
+    x, y = batch
+    y_pred = model(x)
+    loss_val = loss(y_pred, y)
+    optimizer.zero_grad()
+    loss_val.backward()
+    optimizer.step()
+
+
+def preprocess(x: np.ndarray, y: str):
+    x /= 255.0
+    x *= 2
+    x -= 1
+    x = x.reshape((1, 28, 28))
+    return torch.from_numpy(x), torch.tensor(int(y), dtype=torch.long)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)    
+    task = LearningTask(
+        name="test",
+        dataset="mnist",
+        preprocess=preprocess,
+        train_step=train_step,
+        dataloader={"batch_size": 256, "shuffle": True, "drop_last": True},
+        total_epoch=1,
+        members=["2", "3"],
+        merge_iter=20,
+    )
+    node = DeltaNode("http://127.0.0.1:6701")
+    node.create_task(task)
