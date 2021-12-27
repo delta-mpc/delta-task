27687854648fda32cf6428d5dfd383e2e529f810
hmiao <739025250@qq.com>
2021-12-27 10:55:03 +0800

    [feature] change example

--- example.py
+++ example.py
@@ -1,12 +1,9 @@
-import logging
 from typing import Dict, Iterable, List, Tuple, Any, Union
 
-import numpy as np
 import torch
 
-import delta
 from delta import DeltaNode
-from delta.task import HorizontolTask
+from delta.task import HorizontalTask
 from delta.algorithm.horizontal import HorizontalAlgorithm, FedAvg
 
 
@@ -34,39 +31,26 @@ class LeNet(torch.nn.Module):
         x = self.dense2(x)
         return x
 
-
-model = LeNet()
-loss = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(
-    model.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-3, nesterov=True
-)
-
-
-def train_step(batch, model=model, loss=loss, optimizer=optimizer):
-    x, y = batch
-    y_pred = model(x)
-    loss_val = loss(y_pred, y)
-    optimizer.zero_grad()
-    loss_val.backward()
-    optimizer.step()
-
-
-def preprocess(x: np.ndarray, y: str):
-    x /= 255.0
-    x *= 2
-    x -= 1
-    x = x.reshape((1, 28, 28))
-    return torch.from_numpy(x), torch.tensor(int(y), dtype=torch.long)
-
-
-class ExampleTask(HorizontolTask):
+class ExampleTask(HorizontalTask):
     def __init__(self):
-        super().__init__(name="example", dataset="mnist", max_epochs=2, validate_interval=1, validate_frac=0.1)
+        super().__init__(
+            name="example",
+            dataset="mnist",
+            max_rounds=2,
+            validate_interval=1,
+            validate_frac=0.1,
+        )
         self.model = LeNet()
         self.loss_func = torch.nn.CrossEntropyLoss()
-        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-3, nesterov=True)
-    
-    def preprocess(self, x, y = None):
+        self.optimizer = torch.optim.SGD(
+            self.model.parameters(),
+            lr=0.1,
+            momentum=0.9,
+            weight_decay=1e-3,
+            nesterov=True,
+        )
+
+    def preprocess(self, x, y=None):
         x /= 255.0
         x *= 2
         x -= 1
@@ -107,18 +91,26 @@ class ExampleTask(HorizontolTask):
         return list(self.model.parameters())
 
     def algorithm(self) -> HorizontalAlgorithm:
-        return FedAvg(merge_interval_epoch=1, wait_timeout=60, connection_timeout=60)
+        return FedAvg(
+            merge_interval_epoch=0,
+            merge_interval_iter=20,
+            wait_timeout=60,
+            connection_timeout=10,
+            min_clients=2,
+            max_clients=2,
+        )
 
-    def dataloader_config(self) -> Union[Dict[str, Any], Tuple[Dict[str, Any], Dict[str, Any]]]:
+    def dataloader_config(
+        self,
+    ) -> Union[Dict[str, Any], Tuple[Dict[str, Any], Dict[str, Any]]]:
         train_config = {"batch_size": 64, "shuffle": True, "drop_last": True}
         val_config = {"batch_size": 64, "shuffle": False, "drop_last": False}
         return train_config, val_config
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
-    task = ExampleTask()
 
-    delta.debug(task)
+DELTA_NODE_API = "http://127.0.0.1:6700"
 
-    # delta_node = DeltaNode("http://127.0.0.1:6701")
-    # delta_node.create_task(task)
+task = ExampleTask()
+
+delta_node = DeltaNode(DELTA_NODE_API)
+delta_node.create_task(task)
