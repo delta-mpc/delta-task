8fb60edac3aa40cd89cedfd341768101bd7b8a76
hmiao <739025250@qq.com>
2022-05-06 21:42:58 +0800

    remove import in root package

--- delta/__init__.py
+++ delta/__init__.py
@@ -1,14 +1,3 @@
-from tempfile import TemporaryDirectory
-
 from .delta_node import DeltaNode
-from .node import DebugNode
-from .task import Task
 
-__all__ = ["DeltaNode", "debug", "Task"]
-
-
-def debug(task: Task):
-    with TemporaryDirectory() as tmp_dir:
-        for round in range(1, 3):
-            debug_node = DebugNode("1", round, tmp_dir)
-            task.run(debug_node)
+__all__ = ["DeltaNode"]
