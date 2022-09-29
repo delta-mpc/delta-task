ba2a61f0d234b91158a712ac3101eb4251b9d7cf
hmiao <739025250@qq.com>
2022-09-29 22:12:03 +0800

    update version to 0.7.0rc1

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ class PyTest(TestCommand):
 
 setup(
     name="delta-task",
-    version="0.7.0.dev0",
+    version="0.7.0rc1",
     license_files=("LICENSE"),
     packages=find_packages(),
     include_package_data=True,
